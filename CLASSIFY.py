1218561b51d72435c0e94469b0401a446df61775
Denver Cope <Denver.cope@gmail.com>
2025-10-07 11:54:10 +1100

    GRU Added, cleaned up file structure

--- CLASSIFY.py
+++ CLASSIFY.py
@@ -1,9 +1,14 @@
 import joblib
 import sys
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
 
 #Calling models once here  to avoid calling multiple times 
-NBModel = joblib.load("NaiveBayesModel.pkl")
-NBVectorizer = joblib.load("NaiveBayesVectorizer.pkl")
+NBModel = joblib.load(r"ModelTraining/NaiveBayesModel.pkl")
+NBVectorizer = joblib.load(r"ModelTraining/NaiveBayesVectorizer.pkl")
+GRUModel = load_model(r"ModelTraining/GRUModelEmail.h5")
+tokenizer = joblib.load(r"ModelTraining/tokenizer.pkl")
 
 #___SVM_MODEL___
 def classify_email_with_svm(email_text: str) -> str:
@@ -41,7 +46,17 @@ def Classify_SMS_NB(message: str, NBModel, NBVectorizer) -> tuple:
     label = "Spam" if predicted_class == 1 else "Safe"
     #confidence = (confidence * 100)
     return label, confidence
+#GRU Model
+def classify_Email_GRU(message: str, GRUmodel, tokenizer, maxlen=100) -> tuple:
+    sequence = tokenizer.texts_to_sequences([message])
+    padded = pad_sequences(sequence, maxlen=maxlen, padding='post')
 
+    probability = GRUmodel.predict(padded, verbose=0)[0][0]
+    
+    label = "Spam" if probability > 0.5 else "Safe"
+    confidence = probability if label == "Spam" else 1 - probability
+    
+    return label, confidence
 
 def main(args):
     if len(args) > 2:
@@ -52,13 +67,15 @@ def main(args):
             case ("LR"):
                 print ("SMS message classified using Logistic Regression:")
                 print("[",args[2],"] is ", classify_sms_with_lr(args[2])[0], " Confidence(%): ",classify_sms_with_lr(args[2])[1])
-            case ("NB"):
-                #could add an option here to select SMS/Emails? if we want to
+            case ("NB"):#could add an option here to select SMS/Emails? if we want to
                 print ("SMS classified using Naive Bayes Classifier model:")
                 label, confidence = Classify_SMS_NB(args[2], NBModel, NBVectorizer)
-                
                 print(f"[{args[2]}] is {label} \nConfidence(%) in this answer: {confidence:.1%}")
-                
+            case ("GRU"):
+                print ("Email classified using GRU Model:")
+                label, confidence = classify_Email_GRU(args[2], GRUModel, tokenizer)
+                print(f"[{args[2]}] is {label} \nConfidence(%) in this answer: {confidence:.1%}") #can remove confidence values, just added them since both models give a score between 0-1
+
             case ("KM"):
                 # add k-means output
                 return
