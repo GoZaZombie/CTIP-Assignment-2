dae6fb1e2d5c7956827f8f425888532c6c8d5548
DouglasMassey <166668223+DouglasMassey@users.noreply.github.com>
2025-11-06 13:57:59 +1100

    fixed scrolling issue and added better visuals

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -6,6 +6,8 @@ const message = ref('')
 const predictionLabel = ref('')
 const confidence = ref<number | null>(null)
 const error = ref('')
+import emailIcon from '@/assets/email.png'
+import smsIcon from '@/assets/sms.png'
 const resultColor = ref('orange')
 
 const modelOptions = ['LR', 'NBE', 'NBSMS', 'SVM', 'GRU']
@@ -20,6 +22,7 @@ interface Result {
 }
 const results = reactive<Result[]>([]);
 function addResult(type: string, message: string, model: string, prediction: string, confidence: number | null)  {
+  if(message === 'sms') {type = 'sms';};
   results.push({
     id: Date.now(),
     type,
@@ -72,8 +75,9 @@ async function submitForm() {
   } catch (err: unknown) {
     error.value = err instanceof Error ? err.message : 'An unknown error occurred'
   }
-  addResult("not done yet", message.value, modelChoice.value, predictionLabel.value, confidence.value);
+  addResult("email", message.value, modelChoice.value, predictionLabel.value, confidence.value);
 }
+
 </script>
 
 <template>
@@ -85,13 +89,25 @@ async function submitForm() {
       safe: result.prediction === 'Safe'
     }">
     <p class="message">
-      <img src="@/assets/email.png" alt="email icon" width="50">
-      {{ result.message }}
+      <img :src="result.type === 'email' ? emailIcon : smsIcon" 
+           alt="icon" 
+           width="50" />
+      {{ result.message }}<br />
     </p>
-    <p class="metrics">
-      Prediction using {{ result.model }} model: {{ result.prediction }} <br />
-      <span v-if="result.confidence !== null"> (Confidence: {{ result.confidence.toFixed(2) }})</span>
-    </p>
+    <div class="metrics">
+      <p>Prediction using {{ result.model }} model: {{ result.prediction }} <br />
+       <span v-if="result.confidence !== null"> Confidence: {{ (result.confidence*100).toFixed(0) }}%</span>
+      </p>
+      <div class="bar" v-if="result.confidence !== null">
+      <div class="bar_fill" :style="{ '--final-width': (result.confidence*100) + '%'}" :class="{
+      spambar: result.prediction === 'Spam',
+      safebar: result.prediction === 'Safe'
+      }">
+      </div>
+      </div>
+       
+
+    </div>
   </div>
   </div>
 
